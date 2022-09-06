c5c34c01777fd4ea7d92747f63afcfd6b7b96309
unknown <429282-23>
2022-09-06 13:54:59 +0300

    Commit_5_test

---
+++ LW_9/lab9/lab9/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(15, 114);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Реальное время";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Дата";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aqua;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(244, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Возраст пользователя";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(241, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Результат";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Введите дату рождения:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(430, 168);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Узнать возраст пользователя";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LW_9/lab9/lab9/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime d;
+            d = DateTime.Now;
+            label1.Text = "Сегодняшняя дата: " + d.ToLongDateString();
+            label2.Text = "Реальное время: " +  d.ToLongTimeString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DateTime d_Now, d_User;
+
+                // сегодняшняя дата
+                d_Now = DateTime.Now;
+                // ввод даты рождения 
+                d_User = Convert.ToDateTime(maskedTextBox1.Text);
+
+                if (d_Now < d_User)
+                {
+                    label3.Text = " Вы ещё не родились. Введите дату правильно!";
+                }
+                else
+                {
+                    // Проверка на возраст (в нынешнем году прошел день рождения, или нет)
+                    if (d_Now.Month < d_User.Month)
+                    {
+                        if ((d_Now.Year - d_User.Year - 1)%10 == 1) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year - 1).ToString() + " год";
+                        else if ((d_Now.Year - d_User.Year - 1) % 10 > 1 && (d_Now.Year - d_User.Year - 1) % 10 < 5) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year - 1).ToString() + " года";
+                        else label3.Text = "Пользователю " + (d_Now.Year - d_User.Year - 1).ToString() + " лет";               
+                    }
+                    else if(d_Now.Month == d_User.Month)
+                    {
+                        if (d_Now.Day > d_User.Day)
+                        {
+                            if ((d_Now.Year - d_User.Year - 1) % 10 == 1) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year).ToString() + " год";
+                            else if ((d_Now.Year - d_User.Year - 1) % 10 > 1 && (d_Now.Year - d_User.Year - 1) % 10 < 5) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year).ToString() + " года";
+                            else label3.Text = "Пользователю " + (d_Now.Year - d_User.Year).ToString() + " лет";
+                        }
+                        else
+                        {
+                            if ((d_Now.Year - d_User.Year - 1) % 10 == 1) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year - 1).ToString() + " год";
+                            else if ((d_Now.Year - d_User.Year - 1) % 10 > 1 && (d_Now.Year - d_User.Year - 1) % 10 < 5) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year - 1).ToString() + " года";
+                            else label3.Text = "Пользователю " + (d_Now.Year - d_User.Year - 1).ToString() + " лет";
+                        }
+                    }
+                    else
+                    {
+                        if ((d_Now.Year - d_User.Year - 1) % 10 == 1) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year).ToString() + " год";
+                        else if ((d_Now.Year - d_User.Year - 1) % 10 > 1 && d_Now.Year - d_User.Year < 5) label3.Text = "Пользователю " + (d_Now.Year - d_User.Year).ToString() + " года";
+                        else label3.Text = "Пользователю " + (d_Now.Year - d_User.Year).ToString() + " лет";
+                    }                     
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка ввода!", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Запрет на изменени размера окна формы
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+    }
+}
